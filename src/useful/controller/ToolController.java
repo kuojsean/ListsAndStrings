54666335c2de7a1ca53c9f1536268c926b7437aa
kuojsean <kuo.j.sean@gmail.com>
2017-10-03 08:55:51 -0600

    Imported and initialized list and created 3 comments on what certain parts do.

--- src/useful/controller/ToolController.java
+++ src/useful/controller/ToolController.java
@@ -1,10 +1,26 @@
 package useful.controller;
 
+import java.util.List;
+import java.util.ArrayList;
+import useful.model.Donut;
+
+//imported the model and list functions
+
 public class ToolController
 {
-
+	private List<Donut> donutList;
+	
+	public ToolController()
+	{
+		donutList = new ArrayList<Donut>();
+	}
+	
+	//Created a list and initialized it in the controller
+	
 	public void start()
 	{
+		Donut temp = new Donut();
 		
+		donutList.add(temp);
 	}
 }
---
+++ src/useful/model/Donut.java
@@ -0,0 +1,16 @@
+package useful.model;
+
+public class Donut
+{
+	private boolean hasSprinkles;
+	private boolean isGlazed;
+	private String shape;
+	private String flavor;
+	private int holeCount;
+	
+	//Created all of the parts that create the donut.
+	public Donut()
+	{
+		
+	}
+}
